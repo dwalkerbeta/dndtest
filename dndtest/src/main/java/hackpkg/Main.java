9af410f08a8cc798255d6043ae6a8086af9548dd
dbeta <dwalker.beta@gmail.com>
2016-10-09 03:48:04 -0400

    gets rid of console output that I don't want

--- dndtest/src/main/java/hackpkg/Main.java
+++ dndtest/src/main/java/hackpkg/Main.java
@@ -1,13 +1,13 @@
 package hackpkg;
 
 import java.util.List;
+import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.bson.Document;
 import static com.mongodb.client.model.Filters.*;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
@@ -17,33 +17,27 @@ import clarifai2.dto.model.output.ClarifaiOutput;
 import clarifai2.dto.prediction.Concept;
 
 public class Main {
-
-	public Main() {
-		
+	
+	public static String getImageUrl(String url){
+		return url;
 	}
 
 	public static void main(String[] args) {
+		
+		java.util.logging.Logger.getLogger("org.mongodb.driver").setLevel(Level.SEVERE);
+		
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		
 		MongoDatabase db = mongoClient.getDatabase("dnd");
 		
 		MongoCollection<Document> collection = db.getCollection("monsters");
 		
-		MongoCursor<Document> cursor = collection.find().iterator();
-		
-		try {
-		    while (cursor.hasNext()) {
-		        System.out.println(cursor.next().toJson());
-		    }
-		} finally {
-		    cursor.close();
-		}
-		
 		final ClarifaiClient client = new ClarifaiBuilder("ZRGRVhSvL-ns5ZSLMRlr2rB8l_hgwrL1OuQBiukg", "xRjh5V5UwPoxBhhtW5DhJWKVla7pCFYHWvWfzmqh").buildSync();
 
 		client.getToken();
-		
-		String url = "http://cdn.quotationof.com/images/ape-quotes-5.jpg";
+				
+		String url = getImageUrl(args[0].toString());
+		//test String url = getImageUrl("http://cdn.quotationof.com/images/ape-quotes-5.jpg");
 		
 		final List<ClarifaiOutput<Concept>> predictionResults =
 			    client.getDefaultModels().generalModel() // You can also do client.getModelByID("id") to get custom models
@@ -63,10 +57,10 @@ public class Main {
 		String[] arrayOfKeys = new String[18];
 	    int i = 0;
 		while(matcher.find() && i < 18){
-			System.out.println(matcher.group(1));
 			arrayOfKeys[i] = matcher.group(1);
 			arrayOfKeys[i] = arrayOfKeys[i].substring(0, 1).toUpperCase() + arrayOfKeys[i].substring(1);
 			i++;
+			
 		}
 		
 		Document myDoc = collection.find().first();
@@ -78,9 +72,11 @@ public class Main {
 			if(myDoc != null) {
 				System.out.println(myDoc);
 			}
+			
 		}
 		
 		mongoClient.close();
+		System.exit(0);
 		
 	}
 

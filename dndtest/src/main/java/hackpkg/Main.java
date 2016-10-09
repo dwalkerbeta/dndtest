1a2fcb8b3e1cb4cabd71341f58931f728768a590
dbeta <dwalker.beta@gmail.com>
2016-10-08 22:31:16 -0400

    clarifai API success and regexed the returned string so prints each tag

--- dndtest/src/main/java/hackpkg/Main.java
+++ dndtest/src/main/java/hackpkg/Main.java
@@ -1,10 +1,20 @@
 package hackpkg;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
 
 public class Main {
 
@@ -29,7 +39,34 @@ public class Main {
 		}
 						
 		mongoClient.close();
+		
+		final ClarifaiClient client = new ClarifaiBuilder("ZRGRVhSvL-ns5ZSLMRlr2rB8l_hgwrL1OuQBiukg", "xRjh5V5UwPoxBhhtW5DhJWKVla7pCFYHWvWfzmqh").buildSync();
 
+		client.getToken();
+		
+		String url = "http://vignette1.wikia.nocookie.net/forgottenrealms/images/f/f8/Monster_Manual_5e_-_Bugbear_-_p33.jpg/revision/latest?cb=20141109231300";
+		
+		final List<ClarifaiOutput<Concept>> predictionResults =
+			    client.getDefaultModels().generalModel() // You can also do client.getModelByID("id") to get custom models
+			        .predict()
+			        .withInputs(
+			            ClarifaiInput.forImage(ClarifaiImage.of(url))
+			        )
+			        .executeSync()
+			        .get();
+		
+		String names = predictionResults.get(0).toString();
+		
+		Pattern pattern = Pattern.compile("name=(\\w+),");
+		
+		Matcher matcher = pattern.matcher(names);
+		
+		while(matcher.find()){
+			System.out.println(matcher.group(1));
+		}
+
+		
+		
 	}
 
 }
